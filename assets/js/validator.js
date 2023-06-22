7cc323264182239b3fc9a3658653964457e8121b
Kimmerling <leskinglie@gmail.com>
2023-06-22 16:59:49 +0300

    Improved project

--- assets/js/validator.js
+++ assets/js/validator.js
@@ -3,10 +3,27 @@ window.onload = ()=>{
     myForm.addEventListener('submit',function(e){
         let email = document.getElementById('email');
         if(email.value ==''){
+            e.preventDefault();
             let invalid = document.getElementById('invalid');
             invalid.innerHTML = 'Le champ email ne peut pas etre vide';
+            invalid.style.color = 'red';
 
         }
+        else {
+
+        }
+        let message = document.getElementById('message');
+        if(message.value ==''){
+            e.preventDefault();
+            let invalid = document.getElementById('invalid');
+            invalid.innerHTML = "This space should not be empty";
+            invalid.style.color = 'blue';
+        }
+        else{
+            
+        }
+
+
         e.preventDefault();
         document.getElementById('popupbox').style.display = 'block';
         window.scrollTo(top);
